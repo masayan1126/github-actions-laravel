28ef1787132a96f0aae6cdf6ce66b5ca08aacfb9
nishigaki <nishigaki@aivick.co.jp>
2021-09-24 21:56:02 +0900

    バーコードのjsを追加

--- my-app/resources/views/welcome.blade.php
+++ my-app/resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
+        <div id="interactive" class="viewport"></div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -134,30 +135,30 @@
         <script>
 
             Quagga.init({
-                inputStream: { type : 'LiveStream' },
-                decoder: {
-                    readers: [{
-                        format: 'ean_reader',
-                        config: {}
-                    }]
-                }
-            }, (err) => {
+                    inputStream: { type : 'LiveStream' },
+                    decoder: {
+                        readers: [{
+                            format: 'ean_reader',
+                            config: {}
+                        }]
+                    }
+                }, (err) => {
 
-                if(!err) {
+                    if(!err) {
 
-                    Quagga.start();
+                        Quagga.start();
 
-                }
+                    }
 
-            });
+                });
 
             Quagga.onDetected((result) => {
 
                 var code = result.codeResult.code;
                 // ここでAjaxを通して配送完了処理をする
+                console.log(code);
 
             });
-
         </script>
 
     </body>
