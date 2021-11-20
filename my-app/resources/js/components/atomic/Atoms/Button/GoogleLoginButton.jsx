b162e9610cb1410be738ae89bad7c47db02e401d
nishigaki <nishigaki@aivick.co.jp>
2021-11-20 09:42:19 +0900

    googleログイン実装

---
+++ my-app/resources/js/components/atomic/Atoms/Button/GoogleLoginButton.jsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useState, useEffect } from 'react'
+
+const GoogleLoginButton = (props) => {
+    return (
+        <div class="flex items-center justify-end mt-4">
+            {' '}
+            <a href="{{ url('auth/google') }}">
+                {' '}
+                  
+                <img
+                    src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png"
+                    style="margin-left: 3em;"
+                />{' '}
+                 {' '}
+            </a>{' '}
+        </div>
+    )
+}
+
+export default GoogleLoginButton
